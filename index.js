4f65b03aaadb932e6653f6d9d4b8b12c27385c37
Naun Telo <nuni-lirda@hotmail.com>
2018-02-27 11:51:55 +0100

    index.js

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ var app = new express();
 
 app.use(express.static(__dirname+"/dist"));
 
+app.get ("/chart",function(req,res){
+	res.redirect("/chart");
+});
+app.get ("/chartEuLek",function(req,res){
+	res.redirect("/chartEuLek");
+});
 app.listen(process.env.PORT || 8080);
+
